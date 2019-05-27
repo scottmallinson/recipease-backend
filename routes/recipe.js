30e38529c1e68e1ffb012ed959a869a932360012
Scott Mallinson <scott@scottmallinson.com>
2019-05-27 22:35:31 +0200

    Removed console logs

--- routes/recipe.js
+++ routes/recipe.js
@@ -12,7 +12,6 @@ router.get(
     console.log(queryString);
     try {
       const recipe = await Recipe.find({ $text: { $search: queryString } }, { score: { $meta: 'textScore' } }).sort({ score: { $meta: 'textScore' } });
-      console.log('search recipes', recipe);
       res.status(200).json(recipe);
     } catch (error) {
       next(error);
@@ -24,8 +23,6 @@ router.post(
   '/search',
   async (req, res, next) => {
     const { searchForItems } = req.body;
-    // const ingredients = searchForItems.map((items) => items.item);
-    // console.log('ingredients', ingredients);
     const regex = [];
     for (var i = 0; i < searchForItems.length; i++) {
       regex[i] = new RegExp(searchForItems[i]);
@@ -37,7 +34,6 @@ router.post(
         .match({ 'ingredients.name': { $in: regex } })
         .group({ _id: { _id: '$_id', 'name': '$name', 'description': '$description' }, matches: { $sum: 1 } })
         .sort({ matches: -1 });
-      console.log('search recipes by ingredients', recipe);
       res.status(200).json(recipe);
     } catch (error) {
       next(error);
@@ -53,7 +49,6 @@ router.post(
     try {
       const recipe = await Recipe.create({ creatorId, name, description, photoUrl, duration, ingredients, instructions, servings });
       recipeId = recipe._id;
-      console.log('create recipe', recipe);
       res.status(200).json(recipe);
     } catch (error) {
       next(error);
@@ -72,7 +67,6 @@ router.put(
     const { _id, name, description, photoUrl, duration, ingredients, instructions, servings } = req.body;
     try {
       const recipe = await Recipe.findOneAndUpdate({ _id }, { $set: { name, description, photoUrl, duration, ingredients, instructions, servings } }, { new: true });
-      console.log('update recipe', recipe);
       res.status(200).json(recipe);
     } catch (error) {
       next(error);
@@ -86,7 +80,6 @@ router.put(
     const { recipeId, userId } = req.body;
     try {
       const save = await User.findOneAndUpdate({ _id: userId }, { $push: { savedRecipes: recipeId } }, { new: true });
-      console.log('save recipe to user', save);
       res.status(200).json(save);
     } catch (error) {
       next(error);
@@ -100,7 +93,6 @@ router.delete(
     const { id: _id } = req.params;
     try {
       const recipe = await Recipe.deleteOne({ _id });
-      console.log('delete recipe by id', recipe);
       if (!recipe) {
         next(createError(404));
       } else {
@@ -118,7 +110,6 @@ router.get(
     const { id: _id } = req.params;
     try {
       const recipe = await Recipe.findById({ _id });
-      console.log('get recipe by ID', recipe);
       res.status(200).json(recipe);
     } catch (error) {
       next(error);
@@ -131,7 +122,6 @@ router.get(
   async (req, res, next) => {
     try {
       const recipe = await Recipe.find();
-      console.log('get all recipes', recipe);
       res.status(200).json(recipe);
     } catch (error) {
       next(error);
--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,6 @@ router.delete(
     const { id: _id } = req.body;
     try {
       const user = await User.deleteOne({ _id });
-      console.log('delete user', user);
       if (!user) {
         next(createError(404));
       } else {
@@ -30,7 +29,6 @@ router.put(
     const { _id, pantry } = req.body;
     try {
       const user = await User.findOneAndUpdate({ _id }, { $set: { pantry } }, { new: true });
-      console.log('update user\'s pantry', user);
       res.status(200).json(user);
     } catch (error) {
       next(error);
@@ -44,7 +42,6 @@ router.put(
     const { _id, password, displayName, biography, photoUrl, measurements } = req.body;
     try {
       const user = await User.findOneAndUpdate({ _id }, { $set: { password, displayName, biography, photoUrl, measurements } }, { new: true });
-      console.log('update user', user);
       res.status(200).json(user);
     } catch (error) {
       next(error);
@@ -58,7 +55,6 @@ router.get(
     const { id: _id } = req.params;
     try {
       await User.findOne({ _id }).populate('createdRecipes').populate('savedRecipes').exec(function (err, user) {
-        console.log('get users\' recipes', user);
         if (err) console.log(err);
         else res.status(200).json(user);
       });
@@ -74,7 +70,6 @@ router.get(
     const { id: _id } = req.params;
     try {
       const user = await User.findById({ _id });
-      console.log('get user by ID', user);
       res.status(200).json(user);
     } catch (error) {
       next(error);
