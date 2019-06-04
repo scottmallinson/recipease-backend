016283a8512d116a17723a4fe90a637f1b78b884
Scott Mallinson <scott@scottmallinson.com>
2019-06-04 16:39:17 +0200

    Deploy style tweaks

---
+++ public/precache-manifest.faf7de297e0d804a63973f55370ac76e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c359d5710c6fd0114a62",
+    "url": "/static/js/main.19c3bca7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f6a7afb5a185870a2bd",
+    "url": "/static/js/2.b08b95e4.chunk.js"
+  },
+  {
+    "revision": "983ac9bc1a52c1f17220e01a3973d98e",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f21aa90a9537cf71c64b8dfb0adcb549.js"
+  "/precache-manifest.faf7de297e0d804a63973f55370ac76e.js"
 );
 
 workbox.clientsClaim();
