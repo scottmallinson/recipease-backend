02f0aefe5e6886f1aecf21da5266a71a888fc3f9
Scott Mallinson <scott@scottmallinson.com>
2019-06-10 19:03:27 +0200

    Redeployment

--- public/precache-manifest.e28384586a6e2b607de88fd0fc4d2f3d.js
+++ public/precache-manifest.e28384586a6e2b607de88fd0fc4d2f3d.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
-    "revision": "2fa057f0e246dab5eb7f",
-    "url": "/static/js/main.971f914b.chunk.js"
+    "revision": "54c422c3926cdcf99f9e",
+    "url": "/static/js/main.c52424c0.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "b1be859251a21fc25fc4",
-    "url": "/static/js/2.1b751702.chunk.js"
+    "revision": "50f82b9cb99f68b789af",
+    "url": "/static/js/2.6922e267.chunk.js"
   },
   {
     "revision": "7ec13aeb68414b9712816d79b7d0ce15",
     "url": "/static/media/logo.7ec13aeb.svg"
   },
   {
-    "revision": "932031b0749850ba8ba8bfb6fd744682",
+    "revision": "e25c7c28c778200c512e42199ae03f22",
     "url": "/index.html"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.c4596bccd967e877ddf4484ddebffe1a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.e28384586a6e2b607de88fd0fc4d2f3d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.skipWaiting()
 workbox.clientsClaim()
